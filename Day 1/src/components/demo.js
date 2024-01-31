024d8b2771f607f4e56fbda892436a8de47bc389
mythilram <113581621+mythilram@users.noreply.github.com>
2024-01-31 16:27:16 +0530

    Add files via upload

---
+++ Day 1/src/components/demo.js
@@ -0,0 +1,8 @@
+console.log(+'');
+console.log(+true);
+console.log(+"Hello");
+
+let a,b
+a=5
+b=2
+console.log(a << b);
---
+++ Day 1/src/components/spinner.jsx
@@ -0,0 +1,7 @@
+import { Loader } from "rsuite";
+const Spinner =()=>{
+    return(
+        <Loader content="Loding..."/>
+    )
+}
+export default Spinner;
